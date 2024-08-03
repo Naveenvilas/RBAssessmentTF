14305524f2031419c57cc095f3a5c1392f9b6beb
naveenvilas <naveen.shpy@gmail.com>
2024-08-03 23:16:58 +0530

    backend upfate

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,19 +1,3 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-terraform-state-bucket"
-  acl    = "private"
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks"
-  hash_key       = "LockID"
-  billing_mode   = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-
 terraform {
   backend "s3" {
     bucket = "rbkassemnet"
