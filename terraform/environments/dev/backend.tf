56835476ff6e7335699e27945cdc3a7ccc5cfa80
naveenvilas <naveen.shpy@gmail.com>
2024-08-03 13:37:12 +0530

    bucketname update

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "RBK-Bucket"
     key    = "dev/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "terraform-state-lock"
--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "RBK-Bucket"
     key    = "prod/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "terraform-state-lock"
